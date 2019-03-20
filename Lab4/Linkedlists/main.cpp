81a5da7247c41c49bb14807b55aa67605cccfb13
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2019-03-20 07:50:48 +0200

    Finished Ex1 & Ex2, but without supporting the char* type

--- Lab4/Linkedlists/main.cpp
+++ Lab4/Linkedlists/main.cpp
@@ -2,35 +2,35 @@
 #include <iostream>
 using namespace std;
 
-//Function prototypes 
+//Function prototypes
 
 ///////////////////////////////////////////////////////////////////////
 
 int main()
 {
-	LinkedList L;	//create an object of class LinkedList
+	LinkedList<int> L_int;	//create an object of class LinkedList
+	LinkedList<double> L_doub;
+	LinkedList<char*> L_str;
 	int val;
 
 	cout<<"\nPlease enter int values to add to the list (-1 to stop):\n";
 	cin>>val;
 	while(val != -1)
 	{
-		L.InsertBeg(val);
+		L_int.InsertBeg(val);
+		L_doub.InsertBeg(val*6.399);
 		cin>>val;
 	}
-	L.PrintList();
-
+	L_int.PrintList();
+	L_doub.PrintList();
 	cout<<" \n ..............  Lab Requirements .............\n";
 	cout<<"\n Check File LinkedList.h and write the required member functions";
 	cout<<"\n Then test these functions by calling them from the main()";
-	L.InsertBefore(10, 50);
-	L.PrintList();
-	int n;
-	cin >> n;
-	L.DeleteLarger(n);
-	L.PrintList();
-	cout<<endl;
+	
+	L_int.Reverse();
+	L_int.PrintList();
+
+	cout << '\n';
 	system("pause");
 	return 0;
 }
-
